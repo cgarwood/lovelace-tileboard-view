269379ce45240c7e81a33414f6c64143ec863b31
Charles Garwood <cgarwood@gmail.com>
2021-03-24 20:07:37 -0400

    fix for default width/height/pos, styling adjustment

--- src/main.ts
+++ src/main.ts
@@ -30,33 +30,46 @@ class TileboardView extends LitElement {
         if (!this.cards) {
             return html``;
         }
-        return html`${this.cards.map((card) => html`
-            <div class="tile" style="${styleMap(this.cardStyles(card))}">
-                ${card}
-            </div>
-        `)}`;
+        return html`
+            <div id="tile-wrapper">
+            ${this.cards.map((card) => html`
+                <div class="tile" style="${styleMap(this.cardStyles(card))}">
+                    ${card}
+                </div>
+            `)}
+            </div>`;
     }
 
     private cardStyles(item) {
-        console.log(item._config.view_layout);
-        const width = item._config.view_layout.width || 1;
-        const height = item._config.view_layout.height || 1;
-        const pos = item._config.view_layout.position || [0,0];
+        const config = item._config;
+
+        let width = 1;
+        let height = 1;
+        let pos = [0,0]
+
+        if (config.view_layout) {
+            width = config.view_layout.width || 1;
+            height = config.view_layout.height || 1;
+            pos = config.view_layout.position || [0,0];
+        }
+
         const tileSize = this._config.view_layout.tile_size;
         const tileMargin = this._config.view_layout.tile_margin;
-        console.log(width, height, pos, tileSize, tileMargin, this._config.view_layout);
         const styles = {
             width: tileSize * width + tileMargin * (width - 1) + 'px',
             height: tileSize * height + tileMargin * (height - 1) + 'px',
             left: pos[0] * tileSize + (tileMargin * pos[0]) + 'px',
             top: pos[1] * tileSize + (tileMargin * pos[1]) + 'px',
         };
-        console.log(styles);
         return styles;
     }
 
     static get styles(): CSSResult {
         return css`
+            div#tile-wrapper {
+                margin: 16px;
+                position: relative;
+            }
             div.tile {
                 position: absolute;
             }
